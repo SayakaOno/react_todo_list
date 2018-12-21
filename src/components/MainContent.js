b787f0ac6637a5d28a5e36222aa6557792e552f1
Sayaka Ono <sayaka.ono.81@gmail.com>
2018-12-20 21:43:35 -0800

    add TODO list component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import TodoItem from './TodoItem';
+import todoData from '../data/TodoData';
 
 class MainContent extends React.Component {
+  state = { todos: todoData };
+
   render() {
+    const todoItems =  this.state.todos.map(item => {
+      return <TodoItem key={item.id} item={item} />;
+    });
+
     return (
-      <main>hello
+      <main style={{margin: 30}}>
+        <div className="ui inverted segment" style={{maxWidth: 400, margin: '0 auto'}}>
+          <div className="ui inverted relaxed divided list">
+            {todoItems}
+          </div>
+        </div>
       </main>
-    )
+    );
   }
 }
 
---
+++ src/data/TodoData.js
@@ -0,0 +1,29 @@
+const todoData = [
+  {
+    id: 1,
+    text: "study React",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "go shopping",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "cook lunch",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "send mail",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "practice English",
+    completed: false
+  }
+]
+
+export default todoData;
