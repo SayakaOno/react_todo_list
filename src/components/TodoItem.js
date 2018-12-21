13ea26e67c3d1ad898579123b50604136b3b28ff
Sayaka Ono <sayakao@goopter.com>
2018-12-21 09:09:03 -0800

    add styles to completed todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,18 @@ class TodoItem extends React.Component {
 
   render() {
     const {id, text, completed} = this.props.item;
+    const completedStyle = {
+      fontStyle: 'italic',
+      color: "#cdcdcd",
+      textDecoration: "line-through"
+    }
     
     return (
       <div className="item">
         <div className="content">
           <div className="header ui checkbox">
-            <input onChange={() => this.props.onChange(id)} type="checkbox" checked={completed}/>
-            <label htmlFor="">{text}</label>
+            <input id={`item${id}`} onChange={() => this.props.onChange(id)} type="checkbox" checked={completed}/>
+            <label style={completed ? completedStyle : null} htmlFor={`item${id}`}>{text}</label>
           </div>
         </div>
       </div>
