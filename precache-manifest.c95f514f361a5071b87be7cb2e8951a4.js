748acff7b3b71795aa8c07991e49d37ab589cdc0
Sayaka Ono <sayakao@goopter.com>
2018-12-24 07:10:12 -0800

    Updates

---
+++ precache-manifest.c95f514f361a5071b87be7cb2e8951a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4123e4db27ec672d1a",
+    "url": "/react_todo_list/static/js/runtime~main.2a4123e4.js"
+  },
+  {
+    "revision": "48b486fa5414521f9e8d",
+    "url": "/react_todo_list/static/js/main.48b486fa.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/react_todo_list/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "48b486fa5414521f9e8d",
+    "url": "/react_todo_list/static/css/main.82181ae0.chunk.css"
+  },
+  {
+    "revision": "8c29195093b0e7cf06de3c31d0a653b7",
+    "url": "/react_todo_list/index.html"
+  }
+];
